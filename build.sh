441930b9a6f408e80a3539a6b55058d78c8e06ab
Robin Winkelewski <w9ncontact@gmail.com>
2018-02-19 00:10:24 +0100

    add build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+repo=wiin
+
+echo "build alpine base"
+docker build -t $repo/alpine ./tools/alpine
+echo "build desktopless"
+docker build -t $repo/desktopless ./dockerfiles/desktopless 
+echo "build remote-viewer"
+docker build -t $repo/remote-viewer ./dockerfiles/remote-viewer 
+echo "build i3"
+linuxkit pkg build -disable-content-trust ./pkg/i3
+echo "build linux-firmware"
+linuxkit pkg build -disable-content-trust ./pkg/linux-firmware
+echo "build x-intel"
+linuxkit pkg build -disable-content-trust ./pkg/X-intel
+echo " build x-qxl"
+linuxkit pkg build -disable-content-trust ./pkg/X-qxl
+echo "docker-skopeo"
+linuxkit pkg build -disable-content-trust ./pkg/docker-skopeo-image-cache
