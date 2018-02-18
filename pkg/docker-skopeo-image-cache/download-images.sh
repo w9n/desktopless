01f107af8cb319509c663d3d209d67a086d94bf9
Robin Winkelewski <w9ncontact@gmail.com>
2018-02-19 00:02:56 +0100

    skopeo: fix image source

--- pkg/docker-skopeo-image-cache/download-images.sh
+++ pkg/docker-skopeo-image-cache/download-images.sh
@@ -5,7 +5,7 @@ for image in $(cat image.list)
 do
 	echo $image
 	mkdir -p ./dl/$image
-
-    skopeo --insecure-policy copy docker://$image dir:./dl/$image
+    #docker pull $image
+    skopeo --insecure-policy copy docker-daemon:$image dir:./dl/$image
 done
 
